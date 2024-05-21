e013673ca5dcc72562b1df1354b0dc2adb2a568f
o245770 <245770@edu.p.lodz.pl>
2024-05-21 23:19:45 +0200

    7.1 poprawienie commitu 7

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -5,31 +5,32 @@ public class defaultCountingOutRhymer {
     public static final int variable = 12;
     private int[] NUMBERS = new int[variable];
 
-    public int constant = -1;
+    public int definedVariable = -1;
+    public static final int constant = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++constant] = in;
+            NUMBERS[++definedVariable] = in;
     }
 
         public boolean callCheck() {
-            return constant == -1;
+            return definedVariable == constant;
         }
         
             public boolean isFull() {
-                return constant == 11;
+                return definedVariable == 11;
             }
         
                 protected int peekaboo() {
                     if (callCheck())
                         return constant;
-                    return NUMBERS[constant];
+                    return NUMBERS[definedVariable];
                 }
             
                     public int countOut() {
                         if (callCheck())
                             return constant;
-                        return NUMBERS[constant--];
+                        return NUMBERS[definedVariable--];
                     }
 
 }
